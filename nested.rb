eb62e789f1376d68c6677c7ccb50a263556289ce
Aicha D <aicha1diallo@gmail.com>
2019-07-14 01:18:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,23 +85,6 @@ programmer_hash =
   }
 }
 
-
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
     
 end
 
